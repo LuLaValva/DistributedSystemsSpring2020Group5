a037903301a5c81db707804441901fdcf562f2ba
Luke LaValva <lulavalva@gmail.com>
2020-04-30 12:06:47 -0400

    Set up file structure to match spmia-chapter1

---
+++ service/src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting service "
+echo "********************************************************"
+java -jar /usr/local/simple-service/@project.build.finalName@.jar

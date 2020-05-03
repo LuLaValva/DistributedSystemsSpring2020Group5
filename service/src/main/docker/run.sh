8577ef1544eb1032c305479a7103cb17a95f2874
Luke LaValva <lulavalva@gmail.com>
2020-05-03 13:55:15 -0400

    removed service and kept just the spmia  stuff

--- service/src/main/docker/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "********************************************************"
-echo "Starting service "
-echo "********************************************************"
-java -jar /usr/local/simple-service/@project.build.finalName@.jar

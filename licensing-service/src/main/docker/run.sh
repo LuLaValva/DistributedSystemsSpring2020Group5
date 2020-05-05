9200f5fa5af0ba3f8dbf6decae8ad92b968fa4d1
Luke LaValva <lulavalva@gmail.com>
2020-05-05 08:00:16 -0400

    Ported chapter 2 instead of 1 - more extensive

--- licensing-service/src/main/docker/run.sh
+++ licensing-service/src/main/docker/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "********************************************************"
-echo "Starting simple-service "
+echo "Starting License Server"
 echo "********************************************************"
-java -jar /usr/local/simple-service/@project.build.finalName@.jar
+java -jar /usr/local/licensingservice/@project.build.finalName@.jar
